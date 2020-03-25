a27511ccc0f22cc4ecba887983b101b0c737da12
Jonathan Chapman <jrchaps@gmail.com>
2020-03-25 16:52:24 -0700

    Updates

---
+++ precache-manifest.fcf84d95ceec3c6193610ede61580571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fca629d7d5a3807d34234a5b82b3fc4",
+    "url": "/cookbook-app/index.html"
+  },
+  {
+    "revision": "ac569a6b7eb406d7143f",
+    "url": "/cookbook-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "ac569a6b7eb406d7143f",
+    "url": "/cookbook-app/static/js/2.7da21bec.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/cookbook-app/static/js/2.7da21bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5789bcb1298c3f7aad14",
+    "url": "/cookbook-app/static/js/main.0b5e45e6.chunk.js"
+  },
+  {
+    "revision": "bb23c511398c56c297f8",
+    "url": "/cookbook-app/static/js/runtime-main.aaa5c88a.js"
+  }
+]);
